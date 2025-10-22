c24ad3c63eba5c6efef643cc0f5d316b0707deaf
Mob-y <serinteddy@gmail.com>
2025-10-22 16:34:33 +0200

    add balbusaur

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PokemonCard() {
+  return (
+    <figure className="PokemonCard">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbizarre"/>
+      <figcaption>Bulbizarre</figcaption>
+    </figure>
+  );
+}
+
+export default PokemonCard;
