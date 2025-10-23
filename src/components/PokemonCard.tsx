b417b1f04f1ff037b52a6b04c01e545f56160b19
Mob-y <serinteddy@gmail.com>
2025-10-23 11:21:34 +0200

    Add components pokemonCard img

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -3,8 +3,38 @@ import React from "react";
 function PokemonCard() {
   return (
     <figure className="PokemonCard">
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbizarre"/>
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+        alt="Bulbizarre"
+      />
       <figcaption>Bulbizarre</figcaption>
+    </figure>
+  );
+}
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+function PokemonCard() {
+  const pokemon = pokemonList[1];
+
+  return (
+    <figure>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
